b39903a453247217737e08e927b3dd0fff05e6bd
gblvil <65240218+gblvil@users.noreply.github.com>
2023-05-05 22:22:40 +0300

    Init

--- Get-LatestVersionChromeDriver.ps1
+++ Get-LatestVersionChromeDriver.ps1
@@ -5,3 +5,14 @@ $ver = $latestVer.Content
 # Download latest version of ChromeDriver
 $downloadChromeDriver = wget "https://chromedriver.storage.googleapis.com/$Ver/chromedriver_win32.zip" -outfile C:\Selenium\chromedriver_win32.zip
 $downloadChromeDriver
+
+# Test ChromeDriver
+$workPath = 'c:\Selenium\'
+
+Add-Type -Path "$($workPath)\WebDriver.dll"
+
+$ChromeDriver = New-Object OpenQA.Selenium.Chrome.ChromeDriver
+
+$URL = 'https://google.com/'
+
+$ChromeDriver.Navigate().GoToURL($URL)
